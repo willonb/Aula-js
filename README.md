fa7b82c3f520e988e2f6fd3dedfac1732694ea94
techkayna <techkayna@gmail.com>
2024-03-26 22:03:30 -0300

    aula js

--- README.md
+++
@@ -1,2 +0,0 @@
-# Aula-js
-estudo Java
---
+++ fundamentos.js
@@ -0,0 +1,57 @@
+// console 
+
+console.log('esse é o método log')
+
+//console.warn ('esse é o método warn')
+
+//console.error('quebrou a aplicação')
+
+console.table('coloque uma array aqui')
+
+console.info('essa é uma informação')
+
+
+//variáeis
+
+var banana = 10
+let maçã = 15
+const uva = 20 
+
+console.log(banana)
+console.log(maçã)
+console.log(uva)
+
+// tipos de dados 
+
+const name = 'william pascini'
+let qtd = 5
+const isapproved = false
+let lastName = null
+let age
+const languages = ['JavaScript', 'PHP', 'Python']
+const user = {email: 'lcs@teste.com', password: 'teste123'}
+
+// typeof mostra o tipo da váriavel 
+
+
+console.log (name)
+console.log ( qtd)
+console.log (isapproved)
+console.log (age)
+console.table(languages)
+console.log (user)
+
+//array numeros 
+
+let dia = [27,12,1999] 
+
+console.table(dia)
+
+//array objeto
+
+let obj = [
+  {id: 'g', name: 'william', age: 24},
+  {id: 'j', name: 'lucas', age: 26}
+]
+
+console.table(obj)
